55f7c33cb79d74779b332c99c5943bbede551c16
FujiApple <fujiapple852@gmail.com>
2022-02-18 18:01:42 +0800

    doc: update `bounded-static-derive` module documentation

--- bounded_static_derive/src/lib.rs
+++ bounded_static_derive/src/lib.rs
@@ -1,43 +1,12 @@
+#![doc(html_root_url = "https://docs.rs/bounded-static-derive/0.1.0")]
 //! Provides the `ToStatic` derive macro.
 //!
-//! The [`ToStatic`] derive macro implements the [`ToBoundedStatic`] and [`IntoBoundedStatic`] traits for any `struct`
+//! The [`ToStatic`] derive macro implements the [`ToBoundedStatic`](https://docs.rs/bounded_static/trait.ToBoundedStatic.html)
+//! and [`IntoBoundedStatic`](https://docs.rs/bounded_static/trait.IntoBoundedStatic.html) traits for any `struct`
 //! and `enum` that can be converted to a form that is bounded by `'static`.
 //!
-//! It support all `struct` flavors (unit, named & unnamed), all `enum` variant flavors (unit, named & unnamed).  It
-//! does not currently support `union`.
-//!
-//! # Examples
-//!
-//! ```rust
-//! # use std::borrow::Cow;
-//! # use std::collections::HashMap;
-//! # use bounded_static::ToStatic;
-//! /// Named field struct
-//! #[derive(ToStatic)]
-//! struct Foo<'a> {
-//!     aaa: Cow<'a, str>,
-//!     bbb: &'static str,
-//!     ccc: Baz<'a>,
-//! }
-//!
-//! /// Unnamed field struct
-//! #[derive(ToStatic)]
-//! struct Bar<'a, 'b>(u128, HashMap<Cow<'a, str>, Cow<'b, str>>);
-//!
-//! /// Unit struct
-//! #[derive(ToStatic)]
-//! struct Qux;
-//!
-//! #[derive(ToStatic)]
-//! enum Baz<'a> {
-//!     First(String, usize, Vec<Cow<'a, str>>),
-//!     Second { fst: u32, snd: &'static str },
-//!     Third,
-//! }
-//! ```
-//!
-//! See the documentation for [`bounded_static`](https://) for details of the [`ToBoundedStatic`] and
-//! [`IntoBoundedStatic`] traits.
+//! The [`ToStatic`] macro should be used via the [`bounded-static`](https://docs.rs/crates/bounded-static) crate
+//! rather than using this crate directly.
 #![warn(clippy::all, clippy::pedantic, clippy::nursery, rust_2018_idioms)]
 #![forbid(unsafe_code)]
 
@@ -50,9 +19,9 @@ mod data_struct;
 
 /// The `ToStatic` derive macro.
 ///
-/// Generate `ToBoundedStatic` and `IntoBoundedStatic` impls for the data item deriving `ToStatic`.
-///
-/// See the root module for documentation and examples.
+/// Generate [`ToBoundedStatic`](https://docs.rs/bounded_static/trait.ToBoundedStatic.html) and
+/// [`IntoBoundedStatic`](https://docs.rs/bounded_static/trait.IntoBoundedStatic.html) impls for the data item deriving
+/// `ToStatic`.
 #[proc_macro_derive(ToStatic)]
 pub fn to_static(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = syn::parse_macro_input!(input as syn::DeriveInput);
